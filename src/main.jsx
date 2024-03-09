25f41d8fe64a4ceff3da1d4ee66e459fadc03f32
Khuyenne <kbui7272@gmail.com>
2024-03-09 11:44:58 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -19,6 +19,7 @@ import Home from "./home/Home.jsx";
 import Blog from "./blog/Blog.jsx";
 import Shop from "./shop/Shop.jsx";
 import SingleProduct from "./shop/SingleProduct.jsx";
+import CartPage from "./shop/CartPage.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -34,6 +35,10 @@ const router = createBrowserRouter([
         path:"shop/:id",
         element: <SingleProduct/>,
       },
+      {
+        path: "/cart-page",
+        element: <CartPage/>
+      }
     ],
   },
 ]);
